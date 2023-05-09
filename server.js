32f70e3bc7dbbf07bfa23fb9180c57b13676c16d
ni3-gaddamwar <131379838+ni3-gaddamwar@users.noreply.github.com>
2023-05-09 18:14:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.get('/hello', function (req, res) {
     res.send(person);
 });
 
-var server = app.listen(7667, function () {
+var server = app.listen(5040, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log("Example app listening at http://localhost:7667", host, port)
+    console.log("Example app listening at http://localhost:5040", host, port)
 });
 
   
